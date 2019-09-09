0b22be05bc9c6ee1bdd25de947cd8a90c4ba2126
Carlo Miguel Dy <carlomigueldy@gmail.com>
2019-09-09 19:45:25 +0800

    tailwindcss and nuxtjs together is cool

--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,22 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        firstNuxtApp
-      </h1>
-      <h2 class="subtitle">
-        My best Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div class="flex flex-col min-h-screen mx-auto text-center justify-center items-center">
+    <div class="font-mono text-white">
+      <div class="card p-10 bg-green-500 
+      border-2 border-blue-800 rounded mb-3">
+          <p class="text-6xl">Hello Nuxtjs</p>
+          <p>I love TailwindCSS</p>
+      </div>
+      <div class="card p-10 bg-gray-800 border-2
+      border-green-500 rounded">
+        <p>I really think that Nuxtjs and TailwindCSS is just really amazing!</p>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
-  }
+  
 }
 </script>
 
@@ -44,34 +26,4 @@ export default {
   @apply min-h-screen flex justify-center items-center text-center mx-auto;
 }
 */
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
